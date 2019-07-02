a208b2fd60aebef9aa9eb18e353096d3ef58ef24
maknetaRo <makneta@gmail.com>
2019-07-02 12:31:39 +0200

    creating basic templates and first views and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = 'blog/index.html'
+
+
+class AboutView(generic.TemplateView):
+    template_name = 'blog/about.html'
