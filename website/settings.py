b9f3a38212edc7aba587915d97a7fd218ec8a53a
maknetaRo <makneta@gmail.com>
2019-07-07 23:18:37 +0200

    adjusting a free website template to Django blog

--- website/settings.py
+++ website/settings.py
@@ -118,4 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'website/static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
