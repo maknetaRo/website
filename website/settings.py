dd00d9cd94804cf6b1cdc4a0b6739ef16e905d2b
maknetaRo <makneta@gmail.com>
2019-07-25 11:05:49 +0200

    adding static folder and adjusting index.html and about.html

--- website/settings.py
+++ website/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'website/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
